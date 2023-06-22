b2ee7e91c8e9f895072d4ce0735d2c5e9c00c266
uoch <chaouch.thameurtc61@gmail.com>
2023-06-22 16:23:56 +0100

    page

---
+++ script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
